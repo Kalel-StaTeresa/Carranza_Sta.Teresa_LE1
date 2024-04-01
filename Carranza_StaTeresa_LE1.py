3743cd75ede214da76e4f96b217ebbc4adbc1eaa
Kalel <23-00401@g.batstate-u.edu.ph>
2024-04-01 18:02:17 +0800

    Update Carranza_StaTeresa_LE1.py

--- Carranza_StaTeresa_LE1.py
+++ Carranza_StaTeresa_LE1.py
@@ -1,9 +1,10 @@
 import os
 
 game_library ={
-    "Donkey Kong": 3, "Super Mario Bros" : 5, "Tetris" : 2
+    1:{"Donkey Kong": 3}, 
+    2:{"Super Mario Bros" : 5}, 
+    3:{"Tetris" : 2}
 }
-
 
 
 game_lib = {}
@@ -32,7 +33,7 @@ def register():
                     "Balance": userbalance,
                     "Points": userpoints
                     } 
-                return menu() 
+                menu() 
             else:
                 print("Password must be atleast 8 characters long")
                 break   
@@ -79,10 +80,77 @@ def adminlogin():
             return
 
 def user_menu(username):
+    while True:
+        try:
+            print(f"Logged in as "[username])
+            print("1. Rent a game")
+            print("2. Return a game")
+            print("3. Top-up Account")
+            print("4. Display inventory")
+            print("5. Redeem free game rental")
+            print("6. Check Points")
+            print("7. Log out")
+            
+            choice = input("Enter your choice: ")
+            
+            if choice == "1":
+                rent_game(username)
+            if choice == "2":
+                return_game(username)
+            if choice == "3":
+                top_up(username)
+            if choice == "4":
+                inventory(username)
+            if choice == "5":
+                redeem(username)
+            if choice == "6":
+                check_point(username)
+            if choice == "7":
+                print("Logging out...Goodbye!")
+                menu()
+            else:
+                print("Please input an option")
+                break
+        except ValueError:
+            print("Wrong input")
+            return
+    
+def rent_game(username):
+    print(game_library.keys)
+def return_game(username):
+    pass
+def top_up(username):
+    pass
+def inventory(username):
+    pass
+def redeem(username):
+    pass
+def check_point(username):
     pass
 
 def admin_menu():
-    
+    while True:
+        try:
+            print("Admin Menu") 
+            print("1. Update Game Details")
+            print("2. Log out")
+            
+            choice = input("Enter your choice: ")
+            
+            if choice == "1":
+                update_menu()
+            if choice == "2":
+                print("Logging out...Goodbye!")
+                menu()
+            else:
+                print("Please input an option")
+                break
+        except ValueError:
+            print("Wrong input")
+            return
+        
+def update_menu():
+    pass
         
         
         
